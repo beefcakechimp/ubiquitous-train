5920a77640a295acb2dc9b0bf42c411eab8a9537
beefcakechimp <136863552+beefcakechimp@users.noreply.github.com>
2026-05-08 23:24:35 -0500

    Add configuration for Cards game

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.window.title    = "Thirsty Cards"
+    t.window.width    = 1280
+    t.window.height   = 720
+    t.window.resizable = false
+    t.version         = "11.4"
+    t.identity        = "thirsty-cards"
+    t.console         = false
+end
